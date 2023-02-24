39ae86fc4b094277e77df6d288a0c72af4ca2de6
yuknig <i.knigavko@gmail.com>
2023-02-24 13:29:30 +0200

    Implement ordering of patch files.
    Add svn commands for local changes revert and update.

--- CommitPatches.py
+++ CommitPatches.py
@@ -29,12 +29,44 @@ def parse_commit_message_from_patch_file(patch_path):
     return msg
 
 
+def parse_patch_num(patch_file_name):
+    patch_num = 0
+    for ch in patch_file_name:
+        digit = ord(ch) - ord('0')
+        if digit < 0 or digit > 9:
+            break
+        patch_num = digit + patch_num*10
+    return patch_num
+
+# return sorted list of items: { patch_num : patch_file_name }
+def get_patch_dict(patch_dir):
+    dir_files = os.listdir(patch_dir)
+
+    dict = {}
+    for file_idx, file_name in enumerate(dir_files):
+        if not file_name.endswith('.patch'):
+            continue
+
+        # extract patch number
+        num = parse_patch_num(file_name)
+        dict[num] = file_name
+
+    sdict = sorted(dict.items())
+    first_patch_num = sdict[0][0];
+    last_patch_num = sdict[-1][0]
+
+    if len(sdict) != (last_patch_num-first_patch_num+1):
+        print('Patch list seems to be incomplete')
+        raise RuntimeError('Missing patches in set')
+
+    return sdict
+
 def process_dir_with_patches(dir):
     if not os.path.isdir(dir):
         raise NotADirectoryError('Wrong folder path passed:' + str(dir))
 
-    dir_files = os.listdir(dir)
-    if len(dir_files) == 0:
+    patch_dict = get_patch_dict(dir)
+    if len(patch_dict) == 0:
         print('No files found at' + str(dir))
         return
 
@@ -45,21 +77,25 @@ def process_dir_with_patches(dir):
         os.mkdir(commit_dir)
     bat_file_path = os.path.join(commit_dir, 'commit.bat')
     if os.path.exists(bat_file_path):
+        print('Warning: rewriting commit.bat')
         os.remove(bat_file_path)
 
-    for file_idx, file in enumerate(dir_files):
-        if not file.endswith('.patch'):
-            continue
+    # write bat file head
+    with open(bat_file_path, 'a') as bat_file:
+        bat_file.writelines(["svn revert -R .\n", "svn up\n\n"])
 
-        patch_path = os.path.join(dir, file)
+    # iterate files
+    for patch_num, patch_file_name in patch_dict:
+
+        patch_path = os.path.join(dir, patch_file_name)
         msg = parse_commit_message_from_patch_file(patch_path)
-        msg_file_name = '{0:03d}.msg'.format(file_idx+1)
+        msg_file_name = '{0:03d}.msg'.format(patch_num)
         msg_file_path = os.path.join(commit_dir, msg_file_name)
         with open(msg_file_path, 'w') as msg_file:
             msg_file.write(msg)
 
         bat_lines = []
-        bat_lines.append(':: apply commit #{} \'{}\'/b\n'.format(file_idx+1, file))
+        bat_lines.append(':: apply patch #{} \'{}\'\n'.format(patch_num, patch_file_name))
         bat_lines.append('svn patch \"{}\" || exit /b\n'.format(patch_path))
         bat_lines.append('svn commit -F \"{}\" || exit /b\n'.format(msg_file_path))
         bat_lines.append('\n')
